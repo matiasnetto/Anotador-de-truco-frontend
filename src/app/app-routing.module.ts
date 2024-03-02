5be7dd3db40d3abfc9d4087d8d73467834d7d3d5
Matias Netto <matiasnetto03@gmail.com>
2024-03-02 18:50:36 -0300

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MatchComponent } from './pages/match/match.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'test', component: MatchComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
