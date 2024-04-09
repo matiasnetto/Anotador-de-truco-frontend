2c58d507cded331c117132fb9327544fef11dd01
Matias Netto <matiasnetto03@gmail.com>
2024-04-09 08:21:17 -0300

    Initial project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatchComponent } from './pages/match/match.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MatchComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
