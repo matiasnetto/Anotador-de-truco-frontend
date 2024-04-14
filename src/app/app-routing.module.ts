703f307750ca270e4eccf74dbc041ecc8484963a
Matias Netto <matiasnetto03@gmail.com>
2024-04-14 19:21:11 -0300

    Added connect page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MatchComponent } from './pages/match/match.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ConnectComponent } from './pages/connect/connect.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'connect', component: ConnectComponent },
   { path: 'test', component: MatchComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/share/navbar/navbar.component';
+import { ConnectComponent } from './pages/connect/connect.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NavbarComponent],
+  declarations: [AppComponent, HomeComponent, NavbarComponent, ConnectComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
