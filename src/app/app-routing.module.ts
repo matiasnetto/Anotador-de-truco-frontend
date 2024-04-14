405819bbf85721dd11632a1966d36f2fe709b137
Matias Netto <matiasnetto03@gmail.com>
2024-04-14 17:25:34 -0300

    Added navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MatchComponent } from './pages/match/match.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [{ path: 'test', component: MatchComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'test', component: MatchComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/share/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
