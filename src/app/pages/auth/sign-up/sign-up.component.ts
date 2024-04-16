4501eb6e61753567465467c8c4689618973b951e
Matias Netto <matiasnetto03@gmail.com>
2024-04-16 15:59:11 -0300

    Added sign up form

--- src/app/pages/auth/sign-up/sign-up.component.ts
+++ src/app/pages/auth/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+  public form = new FormGroup({
+    'username': new FormControl<string>(""),
+    'fullName': new FormControl<string>(""),
+    'password': new FormControl<string>("")
+  })
 
 }
