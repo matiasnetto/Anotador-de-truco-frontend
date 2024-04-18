1eb49abcad84d229e047fbddf00e08a24cbb05e6
Matias Netto <matiasnetto03@gmail.com>
2024-04-18 14:06:21 -0300

    Add form validation to sign up form

--- src/app/pages/auth/sign-up/sign-up.component.ts
+++ src/app/pages/auth/sign-up/sign-up.component.ts
@@ -1,16 +1,44 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent {
-  public form = new FormGroup({
-    'username': new FormControl<string>(""),
-    'fullName': new FormControl<string>(""),
-    'password': new FormControl<string>("")
-  })
+  submitted = false;
 
+  public form = new FormGroup({
+    username: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.pattern(/^\S*$/),
+    ]),
+    fullName: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(4),
+    ]),
+    password: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
+
+  onSubmit(e: Event) {
+    e.preventDefault();
+    this.form.controls.username.markAsTouched();
+    this.form.controls.fullName.markAsTouched();
+    this.form.controls.password.markAsTouched();
+
+    if (this.form.valid) {
+      console.log(this.form.value);
+      this.submitted = true;
+      //TODO Send data to backend
+    }
+
+    console.log('Username', this.form.controls.username.errors);
+    console.log('FullName', this.form.controls.fullName.errors);
+    console.log('Password', this.form.controls.password.errors);
+  }
 }
