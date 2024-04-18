1641daebf40e2801f93db58afd96ce534483f8cb
Matias Netto <matiasnetto03@gmail.com>
2024-04-18 15:07:02 -0300

    Added auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BASE_URL } from '../config/config';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private readonly http: HttpClient) {}
+
+  createNewUser(username: string, fullName: string, password: string) {
+    return this.http.post(BASE_URL + '/auth/register', {
+      username,
+      fullName,
+      password,
+    });
+  }
+
+  loginUser(username: string, password: string) {
+    return this.http.post<{ token: string }>(BASE_URL + '/auth/login', {
+      username,
+      password,
+    });
+  }
+}
